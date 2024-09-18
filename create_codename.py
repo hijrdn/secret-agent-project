df2db54640c3e8ca2143f94530bd6162d69180c0
Jordan <jordantaylorscheduling+GH@gmail.com>
2024-09-17 23:44:09 -0500

    Initial Codename Script
    Python script that prompts the user to answer questions to generate their secret agent codename.

---
+++ create_codename.py
@@ -0,0 +1,13 @@
+# Creates a secret codename for the user
+
+# Prints text to welcome the user
+print("Welcome to the agency. Let's setup your codename.")
+
+# 3 questions for the user
+city = input("Pick a random city: ")
+zipcode = input("Pick two digits from your current zipcode: ")
+dishware = input("Pick a utensil or dishware: ")
+
+# Creates the codename for the user
+print(dishware,zipcode,city)
+
