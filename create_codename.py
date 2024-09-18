ce15ab1c252b280c2df34ba8ea2e1cff6e11917f
Jordan <jordantaylorscheduling+GH@gmail.com>
2024-09-18 00:40:43 -0500

    Update create_codename.py r-m.v.1.0
    Adding a recruiting menu (v.1.0). 
    
    User can opt out of becoming a secret agent & have their memory wiped. Or they can opt in, answer some questions, get a codename, & receive a short menu of options to select.
    
    Future Update: 
    - menu for agent options should be written as a loop so the user can select all menu options as many times as desired.
    - input text should be saved to recall the agent id later
    - input text should be reformatted as an agent id string, no spaces
    - design structure for a class system, so the script can store information for multiple new agents
    - design structure for overall script, so actions can be stored as repeatable functions that are auto-called by the script.

--- create_codename.py
+++ create_codename.py
@@ -1,13 +1,30 @@
 # Creates a secret codename for the user
+# Opportunity for the user to opt out
+start = input("Do you want to become a secret agent? Y or N ")
 
-# Prints text to welcome the user
-print("Welcome to the agency. Let's setup your codename.")
+# Code for answer Y
+if start == "Y":
 
-# 3 questions for the user
-city = input("Pick a random city: ")
-zipcode = input("Pick two digits from your current zipcode: ")
-dishware = input("Pick a utensil or dishware: ")
+    # Prints text to welcome the user
+    print("Welcome to the agency. Let's setup your codename.")
 
-# Creates the codename for the user
-print(dishware,zipcode,city)
+    # 3 questions for the user
+    city = input("Pick a random city: ")
+    zipcode = input("Pick two digits from your current zipcode: ")
+    dishware = input("Pick a utensil or dishware: ")
 
+    # Creates the codename for the user
+    print("Initializing agent profile: ",dishware,zipcode,city)
+
+    # Prompt agent with a menu
+    menu = input("Things move quickly. What do you need? Money or weapons? ")
+    if menu == "money":
+        print("Your bank account balance is $5,000,020.40.")
+    elif menu == "weapons":
+        print("You have two custom 9mm pistols, an M14, and three throwing knives. As well as some surpressors.")
+    else:
+        print("Sorry, this menu only supports two options.")
+        
+# Code for answer N
+else:
+    print("Understandble. You won't remember this. But I will....")
