adb4fb7c76cd27c682ef3c476efd7f14c441520f
Jordan <jordantaylorscheduling+GH@gmail.com>
2024-09-27 15:34:07 -0500

    Variables & Save New User Functions for login.py
    "Finalizing" the new_user function by updating variables & functions.
    
    1 - added variables & default values that should be associated with each new user profile.
    
    2 - push variables values into a user_profile dictionary which will become user profile save file.
    
    3 - added a function (need to create) for saving the user_profile to a json.
    
    4 - added a function for pushing the codename & password to the users dictionary which will be used for writing to json.
    
    5 - added a function for writing users & passwords dictionary to json (need to create).
    
    6 - error catch message & notification to user that the profile was created.

--- login.py
+++ login.py
@@ -40,3 +40,32 @@ class Login:
             
         #next prompt so user can be referred to by last name
         self.last_name = input("Enter your last name: ")
+
+        #additional variables and default values that belong in the new user profile
+        bank_balance = "10000"
+        mission_count = "0"
+        fr_shots = "0"
+        fr_hits = "0"
+        fr_score = "o"
+        weapons = ["Glock 17", "Throwing Knife"]
+
+        #turn profile variables into a dictionary for use in json
+        user_profile = {
+            "last_name": self.last_name,
+            "codename": self.codename,
+            "password": self.password,
+            "bank_balance": bank_balance,
+            "mission_count": mission_count,
+            "fr_shots": fr_shots,
+            "fr_hits": fr_hits,
+            "fr_score": fr_score,
+            "weapons": weapons,}
+
+        #function for creating a save-file for the user; needs to be written
+        self.save_profile(self.codename, user_profile)
+        #function for associating codename and password with each other in the users dictionary
+        self.users[self.codename] = self.password
+        #function for writing updated users dictionary to json
+        self.write_users()
+        #notification for user that profile is created; also for error catching
+        print(f"Profile for Agent {self.last_name} initiated successfully...")
