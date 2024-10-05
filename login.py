55d98cb01bce919138b883bc09983f294ecf7908
Jordan <jordantaylorscheduling+GH@gmail.com>
2024-10-04 23:52:01 -0500

    Optimize login.py with single json for user file save
    Condenses the user_profile reference to a single file (users.json).

--- login.py
+++ login.py
@@ -1,10 +1,7 @@
-#import json 
 import json
 
-#Create Login Class
 class Login:
-    def __init__(self):
-        #need a list of existing users if any; will need to create a function for this later
+    def __init__(self):        
         self.users = self.read_users()
         self.codename = None
         self.password = None
@@ -12,16 +9,20 @@ class Login:
         
     def prompt_user(self):
         uStatus = input("Do you have a username? (Y / N)\n")
-            uJoin = input("Do you want to become a secret agent? (Y / N)\n")
-            if uJoin == "N":
-                print("Goodbye...you won't remember any of this.\n")
-                return
+            if uStatus.upper() == "N":
+                uJoin = input("Do you want to become a secret agent? (Y / N)\n")
+                if uJoin.upper() == "N":
+                    print("Goodbye...you won't remember any of this.\n")
+                    return None
+                else:
+                    return self.new_user()
+            elif uStatus.upper() == "Y":
+                return self.existing_user()
             else:
-                self.new_user()
-        else:
-            self.existing_user()
+                print("Invalid option. Please enter 'Y' or 'N'.\n")
 
     def new_user(self):
+        #add while loop
         uCity = input("Enter a random city: ").replace(" ", "").upper()
         uZip = input("Enter two digits: ")
         uChar = input("Enter two letters: ").upper()
@@ -34,46 +35,34 @@ class Login:
             
         self.last_name = input("Enter your last name: ")
 
-        bank_balance = "10000"
-        mission_count = "0"
-        fr_shots = "0"
-        fr_hits = "0"
-        fr_score = "o"
-        weapons = ["Glock 17", "Throwing Knife"]
-
-        user_profile = {
+        #condense user profile into users.json
+        self.users[self.codename] = {
             "last_name": self.last_name,
             "codename": self.codename,
             "password": self.password,
-            "bank_balance": bank_balance,
-            "mission_count": mission_count,
-            "fr_shots": fr_shots,
-            "fr_hits": fr_hits,
-            "fr_score": fr_score,
-            "weapons": weapons,}
-
-        #function for creating a save-file for the user; needs to be written
-        self.save_profile(self.codename, user_profile)
-        #function for associating codename and password with each other in the users dictionary
-        self.users[self.codename] = self.password
-        #function for writing updated users dictionary to json
+            "bank_balance": "10000",
+            "mission_count": "0",
+            "fr_shots": "0",
+            "fr_hits": "0",
+            "fr_score": "0",
+            "weapons": {'Glock 17': "1", 'Throwing Knife': "2"},
+            "mission_info": {}}
+        
         self.write_users()
-        #notification for user that profile is created; also for error catching
         print(f"Profile for Agent {self.last_name} initiated successfully...")
+        return self.users[self.codename]
 
     def existing_user(self):
         self.codename = input("Codename: ")
         self.password = input("Password: ")
 
         if self.codename in self.users:
-            if self.password == self.users[self.codename]:
+            if self.password == self.users[self.codename]["password"]:
                 #set profile variables to json dictionary for codename
-                #needs a load_profile function
-                user_profile = self.load_profile(self.codename)
-                #load last_name from dictionary
-                self.last_name = user_profile.get("last_name")
+                user_profile = self.users(self.codename)
                 #print a confirmation method
-                print(f"Welcome back, Agent {self.last_name}.")
+                print(f"Welcome back, Agent {user_profile['last_name']}.")
+                return user_profile
             else:
                 print("Incorrect password...")
         else:
